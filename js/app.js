6cd631c6edaeaf76320168c25fb2d9d91bfb6a45
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2018-05-19 18:41:43 -0300

    Added comments to app.js

--- js/app.js
+++ js/app.js
@@ -1,51 +1,72 @@
 class Enemy {
+    
+    // Enemies constructor function, sets enemie sprite and calls setUp function
     constructor(){
         this.sprite = 'images/enemy-bug.png';
+        this.setUp();
+    }
+
+    // sets random speed the new enemie, and randomly chose one row to put it;
+    setUp() {
         this.speed = 100 + Math.random() * 300;
         this.y = 45 + (85 * Math.floor(Math.random() * 3));
         this.x = -200;
     }
 
+    
+    // Moves the enemy based on its "speed" atribute, than checks if it went off screen,
+    // setting it up again;
     update(dt) {
         this.x += this.speed * dt;
         if (this.x >= 600) {
-            this.x = -200;
-            this.y = 45 + (85 * Math.floor(Math.random() * 3));
-            this.speed = 100 + Math.random() * 300;
+            this.setUp();
         }
     }
 
+    // renders enemie image on the canvas
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
 class Player {
+
+    // Player constructor method, sets player sprite and starting location
     constructor() {
         this.sprite = 'images/char-boy.png';
         this.y = 385;
         this.x = 200;
     }
 
+    // Updates player location, checking if the player collided or reached the water,
+    // than calls the reset function
     update() {
         if (this.y === -40) {
-            this.y =385;
+            this.reset()
         }
     
         for (const enemie of allEnemies) {
             if (enemie.y === this.y) {
                 if ((enemie.x - this.x) >= -50 && (enemie.x - this.x) <= 50){
-                    this.y = 385;
-                    this.x = 200;
+                    this.reset();
                 }
             }
         }   
     }
 
+    // resets player to the starting location
+    reset() {
+        this.y = 385;
+        this.x = 200;
+    }
+
+    // renders player image to the canvas
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
+    // gets by the event listener and checks wich direction key was pressed,
+    // deciding what to do with it
     handleInput(key) {
         switch(key) {
             case 'left':
@@ -72,6 +93,8 @@ class Player {
     }
 }
 
+// initializes the enemies through an immediately evoked function that creates an array of
+// enemies and returns it to the allEnemies array.
 const allEnemies = (() => {
     let arr = [];
     for(let i = 0; i < 6; i++) {
@@ -82,7 +105,7 @@ const allEnemies = (() => {
 })();
 console.log(allEnemies);
 
-
+// initializes player;
 const player = new Player();
 
 // This listens for key presses and sends the keys to your
