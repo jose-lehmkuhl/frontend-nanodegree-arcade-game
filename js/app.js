4d5ef2e637d02eef6cf59620e17cdc3a47385c8f
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2018-05-19 12:33:10 -0300

    Switched app.js from prototype functions to ES6 classes

--- js/app.js
+++ js/app.js
@@ -1,100 +1,76 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.y = 45 + (85 * Math.floor(Math.random() * 3));
-    this.x = -200;
-    this.speed = 100 + Math.floor(Math.random() * 300);
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += this.speed * dt;
-    if (this.x >= 600) {
-        this.x = -200;
-        this.y = 45 + (85 * Math.floor(Math.random() * 3));
+class Enemy {
+    constructor(){
+        this.sprite = 'images/enemy-bug.png';
         this.speed = 100 + Math.floor(Math.random() * 300);
-    }
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-var Player = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/char-boy.png';
-    this.y = 385;
-    this.x = 200;
-};
-
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if (this.y === -40) {
-        this.y =385;
+        this.y = 45 + (85 * Math.floor(Math.random() * 3));
+        this.x = -200;
     }
 
-    for (const enemie of allEnemies) {
-        if (enemie.y === this.y) {
-            if ((enemie.x - this.x) >= -50 && (enemie.x - this.x) <= 50){
-                this.y = 385;
-                this.x = 200;
-            }
+    update(dt) {
+        this.x += this.speed * dt;
+        if (this.x >= 600) {
+            this.x = -200;
+            this.y = 45 + (85 * Math.floor(Math.random() * 3));
+            this.speed = 100 + Math.floor(Math.random() * 300);
         }
     }
-};
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.handleInput = function(key) {
-    switch(key) {
-        case 'left':
-            if(this.x > 0) {
-                this.x -= 100;
-            }
-            break;
-        case 'right':
-            if(this.x < 400) {
-               this.x += 100;
-            }
-            break;
-        case 'up':
-            if(this.y > -40) {
-                this.y -= 85;
-            }
-            break;
-        case 'down':
-            if(this.y < 385) {
-                this.y += 85;
-            }
-            break;
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-};
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+}
 
+class Player {
+    constructor() {
+        this.sprite = 'images/char-boy.png';
+        this.y = 385;
+        this.x = 200;
+    }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+    update() {
+        if (this.y === -40) {
+            this.y =385;
+        }
+    
+        for (const enemie of allEnemies) {
+            if (enemie.y === this.y) {
+                if ((enemie.x - this.x) >= -50 && (enemie.x - this.x) <= 50){
+                    this.y = 385;
+                    this.x = 200;
+                }
+            }
+        }   
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(key) {
+        switch(key) {
+            case 'left':
+                if(this.x > 0) {
+                    this.x -= 100;
+                }
+                break;
+            case 'right':
+                if(this.x < 400) {
+                   this.x += 100;
+                }
+                break;
+            case 'up':
+                if(this.y > -40) {
+                    this.y -= 85;
+                }
+                break;
+            case 'down':
+                if(this.y < 385) {
+                    this.y += 85;
+                }
+                break;
+        }
+    }
+}
 
 const allEnemies = (() => {
     let arr = [];
